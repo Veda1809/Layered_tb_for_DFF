60d2fd09bf2816aa6512ced5068b48fca2e67a4b
Vedavandana Balla <142098395+Veda1809@users.noreply.github.com>
2023-08-24 09:51:19 +0530

    Create driver.sv

---
+++ driver.sv
@@ -0,0 +1,32 @@
+class driver;
+  
+  virtual intf vif;
+  
+  mailbox gen2driv;
+  
+  function new(virtual intf vif,mailbox gen2driv); 
+    this.vif = vif;
+    this.gen2driv = gen2driv;
+  endfunction
+  
+  task main;
+    
+    repeat(1) 
+      begin
+       transaction trans;
+      
+        gen2driv.get(trans);
+       trans.display("Generator");
+       
+        vif.D     <= trans.D;
+        vif.clk     <= trans.clk;
+		vif.reset     <= trans.reset;
+      
+        
+        trans.Q     = vif.Q;      
+        trans.display("Driver");
+        
+    end
+  endtask
+  
+endclass
