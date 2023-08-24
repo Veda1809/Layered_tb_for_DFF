1bacc4a9f773a8fb61c0a46d8e92c6c443b99c16
Vedavandana Balla <142098395+Veda1809@users.noreply.github.com>
2023-08-24 09:54:37 +0530

    Create testbench.sv

---
+++ testbench.sv
@@ -0,0 +1,27 @@
+
+`include "interface.sv"
+`include "test"
+module tbench_top;
+ 
+  bit clk;
+  initial clk = 0;
+ 
+  always #2 clk = !clk ;
+  
+  intf i_intf(clk);
+  test t1(i_intf);
+  
+  DFF_ASRL d1 (
+    .D(i_intf.D),
+    .clk(i_intf.clk),
+    .reset(i_intf.reset),
+    .Q(i_intf.Q)
+   );
+    
+  
+  initial begin 
+    $dumpfile("dump.vcd"); $dumpvars;
+  end
+  
+  
+endmodule
